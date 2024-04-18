fe47c8442508f1a77a749a798626113871403172
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-04-18 13:19:01 +0000

    first program in py

---
+++ 01_Basic/01_hello.py
@@ -0,0 +1,13 @@
+print("hello world!\nIam Sushil")
+
+# How to write comment is py
+# Another way to comment out for multiple code or statement
+'''print("statement 1")
+print("statement 2")'''
+
+# Python Indentation
+
+'''Just like other programming instead of {
+
+}
+space is used in py instead of {}'''
