b02f3eedcf7bd4b4f45a63908c2d3bc1587885a3
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-04-21 16:04:31 +0000

    Basics, input and conditional statement

---
+++ Day2/01_day2.py
@@ -0,0 +1,97 @@
+
+#------------- input() in python --------------
+
+#--- string input ---
+# name=input("name:")
+
+#----- int input ------
+# age=int(input("age:"))
+
+#----- float input -----
+
+# price=float(input("price:"))
+
+# # Taking input from user and printing it
+
+# name=input("Enter name:")
+# age=int(input("Enter age:"))
+# price=float(input("Enter price:"))
+# print("My name is",name, "and I am",age,"years old")
+
+# #write a program to input 2 numbers and print their sum.
+
+# num1=float(input("num1:"))
+# num2=float(input("num2:"))
+
+# sum=num1+num2
+
+# print("The sum of",num1,"and",num2,"is",sum)
+
+# # program3
+# length=float(input("length:"))
+
+# area=length**2
+
+# print("The area is",area)
+
+# # program4
+# num1=float(input("num1:"))
+# num2=float(input("num2:"))
+
+# avearge=(num1+num2)/2
+
+# print("The average of",num1,"and",num2,"is",avearge)
+
+# #program5:WAP to input 2 int numbers, a and b.
+# #print True if a is greater than or equal to b. If not print False
+
+# a=int(input("a:"))
+# b=int(input("b:"))
+
+# if(a>=b):
+#     print("True")      #Identation is important and after : 4 spaces is provided.
+# else:
+#     print("False")
+
+# Conditional Statement
+
+#---------------- if-elif-else -------------------
+
+# if(condition):
+#     statement1
+# elif(condition):
+#     statement2
+# else:
+#     statementN
+
+# Traffic light Code
+
+# light=input("Light color:")
+# if(light=="red"):
+#     print("Stop")
+# elif(light=="green"):
+#     print("Go")
+# elif(light=="yellow"):
+#     print("Look")
+# else:
+#     print("Light is broken")
+
+# ------Single Line if / Ternary Operators--------
+# age=int(input("age:"))
+# message="You can vote" if(age>=18) else "You can't vote"
+# print(message)
+
+# age=int(input("age:"))
+# print("You can vote") if(age>=18) else print("You can't vote")
+
+#-------- Clever if/ Ternary Operator---------
+# age=int(input("age:"))
+# vote=("no","yes") [age>=18] #remember (false_val,true_val)
+# print(vote)
+
+# sal=float(input("salary:"))
+# tax=sal*(0.1,0.2) [sal<=50000]
+# print(tax)
+
+
+
