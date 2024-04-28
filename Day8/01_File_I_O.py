0c9a6ad2043a4fdaf1c60032b4fbe63acb562a59
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-04-28 10:34:36 +0000

    File I/O

--- Day8/01_File_I_O.py
+++ Day8/01_File_I_O.py
@@ -9,19 +9,19 @@
 # Open, read and close File
 #we have to open a file before reading or writing.
 
+# f=open("file_name","mode")
 
 # e.g file_name can be sample.txt , demo.docx etc
 # mode can be r (read mode) ,w (write mode)
 #mode is set to read mode by default
 
-# f=open("file_name","mode")
 # data=f.read()
 # f.close()
 
 
 #example
-# f=open("08_demo.txt","r")
-# # f=open("08_demo.txt","rt") #same as 23
+# f=open("Day8/08_demo.txt","r")
+# # f=open("Day8/08_demo.txt","rt") #same as 23
 # data=f.read()
 # # data=f.read(5) #read 5 character including space too.
 # print(data)
@@ -33,7 +33,7 @@
 
 # data=f.read() #reads entire file
 # data=f.readline() #reads one line at a time
-# f=open("08_demo.txt")
+# f=open("Day8/08_demo.txt")
 # line1=f.readline()
 # print(line1)
 # line2=f.readline()
@@ -45,33 +45,33 @@
 #-----------Writing to a file------
 #------------w(it overwrite entire file) or a(adds to the file) mode-----------
 
-# f=open("08_demo.txt","w")
+# f=open("Day8/08_demo.txt","w")
 # f.write("I surpass you")
 # f.close()
 
 
-# f=open("08_demo.txt","a")
+# f=open("Day8/08_demo.txt","a")
 # f.write("\nYou are good Observer.")
 # f.close()
 
 #if you do this this will automatically create a new file if not existed
 
-# f=open("08_sample.txt","a")
+# f=open("Day8/08_sample.txt","a")
 # f.close()
 
-# f=open("08_demo.txt","r+")
+# f=open("Day8/08_demo.txt","r+")
 # f.write("abc")
 # print(f.read())
 # f.close()
 
 
-# f=open("08_demo.txt","w+")
+# f=open("Day8/08_demo.txt","w+")
 # # print(f.read())
 # f.write("abc")
 # f.close()
 
 
-# f=open("08_demo.txt","a+")
+# f=open("Day8/08_demo.txt","a+")
 # print(f.read())
 # f.write("abc")
 # f.close()
@@ -84,10 +84,10 @@
 
 
 #-----------with syntax-----------
-with open("08_demo.txt","r") as f:
-    data=f.read()
-    print(data)
-    # f.close() #optional while using with syntax
+# with open("Day8/08_demo.txt","r") as f:
+#     data=f.read()
+#     print(data)
+#     # f.close() #optional while using with syntax
 
 #--------Deleting a File-------
 #using the os module
@@ -97,6 +97,58 @@ with open("08_demo.txt","r") as f:
 
 # example
 import os
-# os.remove("08_demo.txt")
+# os.remove("Day8/08_demo.txt")
 
 #practice set
+ 
+with open("08_practice.txt","w") as f:
+    f.write("Hi everyone\nwe are learning File I/O\nusing Java.\nI like programming in Java")
+
+with open("Day8/08_practice.txt","r") as f:
+    data=f.read()
+def replace():
+    new_data=data.replace("Java","python")
+    print(new_data)
+    with open("08_practice.txt","w") as f:
+        f.write(new_data)
+replace()
+
+
+def check_for_word(word):
+    with open("Day8/08_practice.txt","r") as f:
+        data=f.read()
+        if(word in data):
+            print(word, "exists in the file")
+        else:
+            print(word,"not exist in the file")
+
+check_for_word("learning")
+
+
+def check_for_line():
+    with open("Day8/08_practice.txt","r") as f:
+        word="ok"
+        data=True
+        line_no=1
+        while data:
+            data=f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            else:
+                line_no+=1
+
+    return print(-1)
+check_for_line()
+
+count=0
+with open("Day8/08_number.txt") as f:
+    data=f.read()
+    print(data)
+    nums=data.split(',') #gives list of string
+    print(nums)
+    for val in nums:
+        if(int(val)%2==0):
+            count+=1
+print(count)
+        
