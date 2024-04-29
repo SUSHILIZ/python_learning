84ded02d1eaf611935757afa42c49a7d5d6c4694
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-04-29 16:40:37 +0000

    OOP

--- Day9/02_oop.py
+++ Day9/02_oop.py
@@ -43,3 +43,62 @@ s1=Student("sushil",[90,98,96])
 s1.print_average()
 
 
+
+#----------Static Methods-----------------
+# Methods that don't use the self parameter(work at class level)
+
+class Student:
+    @staticmethod #decorator
+    def hello():
+        print("hello")
+s1=Student()
+s1.hello()
+
+
+# Important
+# ----------------Abstraction-------------
+#-----------------Encapsulation----------
+#-----------------Inheritance------------
+#-----------------Polymorphism-----------
+
+
+#-----------Abstraction-----------
+#Hiding the necessary implementation detail of a class and only showing essential features to the user.
+
+class Car:
+    def __init__(self):
+        self.cla=False
+        self.acc=False
+        self.brk=False
+    def start(self):
+        self.cla=True
+        self.acc=True
+        print("car started")
+car1=Car()
+car1.start()
+
+#-------------Encapsulation-----------
+# Wrapping data and functions into a single unit(object)
+
+#Create Account class with 2 attributes- balance and account no.
+#create methods for debit, credit and printing the balance.
+
+class Account:
+    def __init__(self,balance,accountNo):
+        self.balance=balance
+        self.accountNo=accountNo
+    def debit(self,amount):
+        self.balance-=amount
+        print(self.accountNo,"debited by Rs",amount)
+        print("total balance=",self.bal())
+    def credit(self,amount):
+        self.balance+=amount
+        print(self.accountNo,"credited by Rs",amount)
+        print("total balance=",self.bal())
+
+    def bal(self):
+        return self.balance
+
+acc1=Account(10000,1234567890)
+acc1.debit(100)
+acc1.credit(1000)
